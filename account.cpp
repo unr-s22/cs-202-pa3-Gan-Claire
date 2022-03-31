ecec90448ac29d69a5258beb549cba77950b9b15
SierraScott2112 <sierrascott@nevada.unr.edu>
2022-03-30 19:43:14 -0700

    account class

--- account.cpp
+++ account.cpp
@@ -15,11 +15,9 @@ void Account::makeDeposit(){
         deposit = arr[i];
         //amounts from main
 
-<<<<<<< HEAD
         for(int i=0;i<n;i++) {
             sum+=arr[i];
     }
-=======
         for(int i=0; i<n; i++){
             cin>>arr[i];
     	}
@@ -27,7 +25,6 @@ void Account::makeDeposit(){
         for(int i=0;i<n;i++) {
             sum+=arr[i];
 	}
->>>>>>> 7b2705998f511f612e1a53fb0c1054a23af00925
 
     cout<<"\n total sum amount:"<<sum;
     return sum;
@@ -37,12 +34,9 @@ void Account::makeDeposit(){
 void Account::makeWithdrawls(){
     int n, sum = sum;
     cout<<"total account balance:"<<sum<<endl;
-<<<<<<< HEAD
-=======
     cout<<"How many withdrawls would you like to make"<<endl;
     cin>> n;
     int arr[n];
->>>>>>> 7b2705998f511f612e1a53fb0c1054a23af00925
     cout<<"Enter the total withdrawl amounts";
 
         void withdrawl(const money &m);
@@ -57,9 +51,5 @@ void Account::makeWithdrawls(){
         return 0;
         }
 
-<<<<<<< HEAD
-}
-=======
 }
 
->>>>>>> 7b2705998f511f612e1a53fb0c1054a23af00925
